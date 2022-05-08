cbef371de245cf5324971880c68b62aa003760f1
walidkaffel <walid.kaffel@gmail.com>
2022-05-08 18:18:41 +0100

    Create Main_Parser.py

---
+++ Main_Parser.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  8 16:47:14 2022
+
+@author: walid
+"""
+
+import raw_files_extractor as raw_extractor
+
+def main():
+    raw_extractor.extractor()
+
+if __name__ == "__main__":
+    main()
