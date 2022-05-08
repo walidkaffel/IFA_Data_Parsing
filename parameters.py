e083f5c50a52875ecf5b3cb3f435490a6abcc143
walidkaffel <walid.kaffel@gmail.com>
2022-05-08 18:18:38 +0100

    Create parameters.py

---
+++ parameters.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  8 17:00:26 2022
+
+@author: walid
+"""
+
+raw_files_repo = "E:/Sources/IFA/raw_downloads"
+extracted_files_repo = "E:/Sources/IFA/extracted"
